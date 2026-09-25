7a8b29ad16cd3c731e06a87a8dfedcf38c2e6719
jennifercesar-cloud <jennifer.cesar@escola.pr.gov.br>
2026-09-25 15:30:00 +0000

    frdezrf

---
+++ script.js
@@ -0,0 +1,25 @@
+function calcularMedia() {
+    const textoNota1 = document.getElementById("nota1").value
+    const textoNota2 = document.getElementById("nota2").value
+    const textoNota3 = document.getElementById("nota3").value
+
+
+    const nota1 = number(textoNota1)
+    const nota2 = number(textoNota2)
+    const nota3 = number(textoNota3)
+
+    const media = (nota1 + nota2 + nota3) / 3 
+    let situacao
+    if (media >= 7){
+        situacao = "APROVADO"
+    } else if (media >= 5){
+        situacao = "RECUPERACAO"
+    } else {
+        situacao = "REPROVADO"
+    }
+    const mediaFormatada = media.toFixed(1)
+    document.getElementById("resultado").textContent =
+    "Media: " + mediaFormatada + " - Situação: " + situacao
+
+
+}
